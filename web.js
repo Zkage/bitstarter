4ce9caf3887029a56e111c181dfe21307f6d1e7e
Zeeshan Shah <zeeshanshah@gmail.com>
2013-08-06 17:16:08 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var fs = require('fs');
-var app = express.createServer(express.logger());
+var htmlfile = "index.html";
 
-var htmlimport = fs.readFileSync('index.html');
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-    response.writeHead(200,{ 'Content-type': 'text/html; charset=utf-8'});
+var html = fs.readFileSync(htmlfile).toString();
 
-    response.end(htmlimport);
+
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
